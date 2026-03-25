852ada308d3dc56d1f4090fa81264d15bd3ff65c
byu-learner14 <ocrawford@byupathway.edu>
2026-03-25 11:27:28 -1000

    fixed

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -54,21 +54,21 @@ const temples = [
     location: "St. George, Utah, United States",
     dedicated: "1877, April, 6",
     area: 143969,
-    imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/st-george-utah/400x250/st-george-temple-lds-123456-wallpaper.jpg"   // ← this one still 404 sometimes, use the alternative below if needed
+    imageUrl: "images/st-george-temple-lds.jpg"
   },
   {
     templeName: "Laie Hawaii",
     location: "Laie, Hawaii, United States",
     dedicated: "1919, November, 27",
     area: 42100,
-    imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/laie-hawaii/400x250/laie-hawaii-temple-lds-960217-wallpaper.jpg"  // still may 404
+    imageUrl: "images/laie-hawaii-temple-lds.jpg"
   },
   {
     templeName: "Colonia Juárez Chihuahua Mexico",
     location: "Colonia Juárez, Chihuahua, Mexico",
     dedicated: "1999, March, 6",
     area: 6800,
-    imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/colonia-juarez-mexico/400x250/colonia-juarez-mexico-temple-lds-960216-wallpaper.jpg"  // still may 404
+    imageUrl: "images/colonia-juarez-mexico-temple-lds.jpg"
   }
 ];
 
